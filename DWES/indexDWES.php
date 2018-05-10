471fd93aa7d89a05541affd702ebd94e65b352ff
Luci <daw2@IS32WX08>
2018-05-10 13:07:32 +0200

    COMMIT TEMA 1 DWES

---
+++ DWES/indexDWES.php
@@ -0,0 +1,74 @@
+<?php
+require_once "view/cabecera.php";
+?>
+<div class="container">
+    <div class="row col-md-12">
+        <h1>DESARROLLO WEB EN ENTORNO SERVIDOR</h1>
+    </div>
+    <div class="row col-md-12">
+        <div class="col-md-4 tecnologias">
+            <figure>
+                <a href="tema1/indexTema1.php"><img src="webroot/img/servidor.png" class="img-responsive" alt="" width="72.5%" style="margin-left: 15%"></a>
+                <figcaption><a href="tema1/indexTema1.php">1- Desarrollo web en entorno servidor</a></figcaption>
+            </figure>
+
+        </div>
+        <div class="col-md-3 tecnologias">
+            <figure>
+                <a href="tema2/indexTema2.php"><img src="webroot/img/desarrollo.png" class="img-responsive" alt=""></a>
+                <figcaption><a href="tema2/indexTema2.php">2- Entornos de desarrollo y explotación</a></figcaption>
+            </figure>
+        </div>
+        <div class="col-md-4 tecnologias">
+            <figure>
+                <a href="tema3/indexTema3pag1.php"><img src="webroot/img/php.png" class="img-responsive" alt="" width="72.5%" style="margin-left: 15%"></a>
+                <figcaption><a href="tema3/indexTema3pag1.php">3- Características del lenguaje PHP</a></figcaption>
+            </figure>
+        </div>
+    </div>
+    <div class="row col-md-12">
+        <div class="col-md-4 tecnologias">
+            <figure>
+                <a href="tema4/indexTema4.php"><img src="webroot/img/db.png" class="img-responsive" alt="" width="74%" style="margin-left: 15%"></a>
+                <figcaption><a href="tema4/indexTema4.php">4- Técnicas de acceso a datos en PHP</a></figcaption>
+            </figure>
+        </div>
+        <div class="col-md-3 tecnologias">
+            <figure>
+                <a href="tema5/indexTema5.php"><img src="webroot/img/session.png" class="img-responsive" alt=""></a>
+                <figcaption><a href="tema5/indexTema5.php">5- Aplicaciones web con código embebido</a></figcaption>
+            </figure>
+        </div>
+        <div class="col-md-4 tecnologias">
+            <figure>
+                <a href="tema6/indexTema6.php"><img src="webroot/img/poo.png" class="img-responsive" alt="" style="width: 400px; margin-left: 4%;"></a>
+                <figcaption><a href="tema6/indexTema6.php">6- Programación orientada a objetos en PHP</a></figcaption>
+            </figure>
+        </div>
+    </div>
+    <div class="row col-md-12">
+        <div class="col-md-4 tecnologias">
+            <figure>
+                <a href="tema7/indexTema7.php"><img src="webroot/img/ws.png" class="img-responsive" alt="" width="73%" style="margin-left: 15%"></a>
+                <figcaption><a href="tema7/indexTema7.php">7- Servicios web</a></figcaption>
+            </figure>
+        </div>
+        <div class="col-md-3 tecnologias">
+            <figure>
+                <a href="tema8/index.php"><img src="webroot/img/web-dinamica.png" class="img-responsive" alt="" style="width: 400px;"></a>
+                <figcaption><a href="tema8/index.php">8- Aplicaciones web dinámicas</a></figcaption>
+            </figure>
+
+        </div>
+        <div class="col-md-4 tecnologias">
+            <figure>
+                <a href="../wip.php"><img src="webroot/img/web-hibrida.png" class="img-responsive" alt="" width="73%" style="margin-left: 15%"></a>
+                <figcaption><a href="../wip.php">9- Aplicaciones web híbridas</a></figcaption>
+            </figure>
+
+        </div>
+    </div>
+</div>
+<?php
+require_once "view/pie.php";
+?>
