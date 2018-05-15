f927273b4f9a56a18892629f2c32e51a797d3902
pablocidon <pablo.cidbar@gmail.com>
2018-05-15 16:28:43 +0200

    API Rest Departamentos

--- DWES/tema8/vista/vRest.php
+++ DWES/tema8/vista/vRest.php
@@ -105,11 +105,11 @@
         <div class="alert alert-success alert-dismissable col-sm-10">
             <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
             <?php
-                if(is_null($_SESSION['departamento'])){
-                    echo "No existe el departamento";
+                if(is_null($_SESSION['departamento']) || !is_null($_SESSION['departamento']->FechaBaja)){
+                    echo "No se han obtenido resultados";
                 }else{
                     echo "El volumen de negocio del departamento cuyo código es <strong>".$_POST['CodDepartamento']."</strong> es
-            <strong>".$_SESSION['departamento']->getVolumenDeNegocio()."</strong>";
+            <strong>".$_SESSION['departamento']->Volumen."</strong>";
                 }
             ?>
 
